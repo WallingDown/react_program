3c97c657fbc8a520308330042067bc3d2bd7a6c9
WallingDown <1532147890@qq.com>
2020-08-11 20:47:15 +0800

    异步的action

---
+++ src_异步的action/components/Count.jsx
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+import {incrementAction,decreateAction,incrementWaitAction} from '../redux/count_action'
+ 
+export default class Count extends Component {
+    state = {
+        // count:0
+        name:'小白'
+    }
+
+    // 加
+    increment = () =>{
+        // 1、获取用户输入
+        const {value} = this.refs.selectedNumber
+        // 2、获取原状态
+        // const {count} = this.state
+        // 3、执行后，更新状态
+        // this.setState({count:count + value *1})
+        this.props.store.dispatch(incrementAction(value*1))
+    }
+
+    // 减
+    decrement = () =>{
+         // 1、获取用户输入
+         const {value} = this.refs.selectedNumber
+         // 2、获取原状态
+        //  const {count} = this.state
+         // 3、执行后，更新状态
+        //  this.setState({count:count - value *1})
+        this.props.store.dispatch(decreateAction(value*1))
+    }
+
+    // 奇数加
+    incrementIfOdd = () =>{
+        if (this.props.store.getState() % 2 !== 0) {
+              // 1、获取用户输入
+                const {value} = this.refs.selectedNumber
+                // 2、获取原状态
+                // const {count} = this.state
+                // 3、执行后，更新状态
+                // this.setState({count:count + value *1})
+                this.props.store.dispatch(incrementAction(value*1))
+        }
+    }
+
+    // 等一等再加
+    incrementWait = () =>{
+             // 1、获取用户输入
+             const {value} = this.refs.selectedNumber
+             // 2、获取原状态
+            //  const {count} = this.state
+             // 3、执行后，更新状态
+            //  this.setState({count:count + value *1})
+            this.props.store.dispatch(incrementWaitAction(value*1))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>{this.state.name}当前的和为:{this.props.store.getState()}</h1>
+                <select ref="selectedNumber">
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+                <button onClick={this.increment}>+</button>
+                <button onClick={this.decrement}>-</button>
+                <button onClick={this.incrementIfOdd}>若为奇数 +</button>
+                <button onClick={this.incrementWait}>等一等 +</button>
+            </div>
+        )
+    }
+}
