faef5a5e2bea52713f1fcf00fb7971a48e16c12e
WallingDown <1532147890@qq.com>
2020-08-11 20:45:00 +0800

    更新

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import {incrementAction,decreateAction} from '../redux/count_action'
+import {incrementAction,decreateAction,incrementWaitAction} from '../redux/count_action'
  
 export default class Count extends Component {
     state = {
@@ -44,15 +44,13 @@ export default class Count extends Component {
 
     // 等一等再加
     incrementWait = () =>{
-        setTimeout(()=>{
              // 1、获取用户输入
              const {value} = this.refs.selectedNumber
              // 2、获取原状态
             //  const {count} = this.state
              // 3、执行后，更新状态
             //  this.setState({count:count + value *1})
-            this.props.store.dispatch(incrementAction(value*1))
-        },500)
+            this.props.store.dispatch(incrementWaitAction(value*1))
     }
 
     render() {
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 // 引入createStore，创建redux最核心对象
-import {createStore} from 'redux'
+import {createStore,applyMiddleware} from 'redux'
 // 引入countReducer  加工Count的状态
 import countReducer from './count_reducer'
 // 
-const store = createStore(countReducer)
+import thunk from 'redux-thunk'
+// 
+const store = createStore(countReducer,applyMiddleware(thunk))
 // 暴露出去
 export default store
