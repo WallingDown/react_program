bed46a2ae9252385e77af340c27fa3613aa88bc9
WallingDown <1532147890@qq.com>
2020-08-11 13:47:28 +0800

    redux基础

--- src before_redux/components/Count.jsx
+++ src before_redux/components/Count.jsx
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 
 export default class Count extends Component {
     state = {
-        // count:0
-        name:'小白'
+        count:0
     }
 
     // 加
@@ -11,9 +10,9 @@ export default class Count extends Component {
         // 1、获取用户输入
         const {value} = this.refs.selectedNumber
         // 2、获取原状态
-        // const {count} = this.state
+        const {count} = this.state
         // 3、执行后，更新状态
-        // this.setState({count:count + value *1})
+        this.setState({count:count + value *1})
     }
 
     // 减
@@ -21,21 +20,21 @@ export default class Count extends Component {
          // 1、获取用户输入
          const {value} = this.refs.selectedNumber
          // 2、获取原状态
-        //  const {count} = this.state
+         const {count} = this.state
          // 3、执行后，更新状态
-        //  this.setState({count:count - value *1})
+         this.setState({count:count - value *1})
     }
 
     // 奇数加
     incrementIfOdd = () =>{
-        // if (this.state.count % 2 !== 0) {
+        if (this.state.count % 2 !== 0) {
               // 1、获取用户输入
                 const {value} = this.refs.selectedNumber
                 // 2、获取原状态
-                // const {count} = this.state
+                const {count} = this.state
                 // 3、执行后，更新状态
-                // this.setState({count:count + value *1})
-        // }
+                this.setState({count:count + value *1})
+        }
     }
 
     // 等等再加
@@ -44,16 +43,16 @@ export default class Count extends Component {
              // 1、获取用户输入
              const {value} = this.refs.selectedNumber
              // 2、获取原状态
-            //  const {count} = this.state
+             const {count} = this.state
              // 3、执行后，更新状态
-            //  this.setState({count:count + value *1})
+             this.setState({count:count + value *1})
         },500)
     }
 
     render() {
         return (
             <div>
-                <h1>{this.state.name}当前的和为:xxx</h1>
+                <h1>当前的和为:{this.state.count}</h1>
                 <select ref="selectedNumber">
                     <option value="1">1</option>
                     <option value="2">2</option>
